a4183776cb0373237bfa540ac013f8d4b0d7904f
yeli130 <li130@sheridancollege.ca>
2023-01-26 08:34:56 -0500

    HelloWorld version2

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -9,12 +9,13 @@ package helloworld;
  * @author yeli8
  */
 public class HelloWorld {
-
+  String word;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World");
+        
     }
     
 }
